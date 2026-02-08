bb5f04e92adb651ee4558a628d3614ab826e3e94
IT25101807 <IT25101807@my.sliit.lk>
2026-02-08 04:21:40 +0000

    Lab01 - Question05: Add Calculator Java class

---
+++ Appcal.java
@@ -0,0 +1,43 @@
+class Calculator {
+
+    // add two integers
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // multiply two integers
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // square of an integer
+    public int square(int a) {
+        return a * a;
+    }
+}
+
+public class Appcal {
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+
+        // 1. (3 * 4 + 5 * 7)^2
+        int part1 = calc.multiply(3, 4);
+        int part2 = calc.multiply(5, 7);
+        int sum1 = calc.add(part1, part2);
+        int result1 = calc.square(sum1);
+
+        // 2. (4 + 7)^2 + (8 + 3)^2
+        int sum2 = calc.add(4, 7);
+        int square1 = calc.square(sum2);
+
+        int sum3 = calc.add(8, 3);
+        int square2 = calc.square(sum3);
+
+        int result2 = calc.add(square1, square2);
+
+        // Output results
+        System.out.println("Result of (3*4 + 5*7)^2 = " + result1);
+        System.out.println("Result of (4+7)^2 + (8+3)^2 = " + result2);
+    }
+}
